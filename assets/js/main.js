02d7fa39b7e28925e13c77c282203be413c43c8a
giddyPwild <giddygbenga4@gmail.com>
2022-11-06 21:22:43 +0100

    initial Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,14 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
 });
+
+
+/*===== GSAP ANIMATION =====*/
+// NAV
+// gsap.from('.home__blob, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
+// gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+
+// gsap.from('.home__data', { opacity: 0, duration: 1, delay: 1.6, y: 30 });
+// gsap.from('.home__description', { opacity: 0, duration: 1, delay: 2., y: 30 });
+// gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.4, y: 30})
+// gsap.from('.home__img', { opacity: 0, duration: 1, delay: 1., y: 30 });
